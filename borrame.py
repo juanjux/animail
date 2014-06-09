75a3378bf42f9823b0bb7a891ffee2882d72479c
Juanjo Alvarez <juanjux@almarsoft.com>
2014-06-09 11:43:30 +0200

    cleanup

--- borrame.py
+++
@@ -1,7 +0,0 @@
-###########################################################################
-#    Copyright (C) 2003 by Juanjo Hijo                                      
-#    <juanjux@pascual>                                                             
-#
-# Copyright: See COPYING file that comes with this distribution
-#
-###########################################################################
